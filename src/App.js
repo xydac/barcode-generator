e42ba2610e5d8ecc89b427850c8bb799b9bd4a22
Deepak Seth <deepak.seth@xydac.com>
2021-12-29 14:55:31 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Barcode from "react-barcode";
+
+const BarcodeData = ({ labelTop, labelBottom, value }) => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      border: "1px solid #ddd",
+    }}>
+    {labelTop}
+    <Barcode value={value} />
+    {labelBottom}
+  </div>
+);
 
 function App() {
+  const queryParams = new URLSearchParams(window.location.search);
+  const value = queryParams.get("value") || "Barcode";
+  const labelTop = queryParams.get("labelTop");
+  const labelBottom = queryParams.get("labelBottom");
+  const rows = queryParams.get("rows") || 10;
+  const columns = queryParams.get("columns") || 4;
+  const range = [...Array(rows * columns).keys()];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          margin: "auto",
+          display: "grid",
+          gridGap: "1rem",
+          gridTemplateColumns: `repeat(${columns}, 1fr)`,
+          width: "max-content",
+        }}>
+        {range.map((i) => (
+          <BarcodeData
+            key={i}
+            value={value}
+            labelBottom={labelBottom}
+            labelTop={labelTop}
+          />
+        ))}
+      </div>
     </div>
   );
 }
